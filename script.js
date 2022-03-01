2b7dfbbb0779aa47a0645875b8c02578fae0d7c4
Essan'Utep Blue <eblue.codes@gmail.com>
2022-02-28 22:23:00 -0700

    I created the logic that will allow the computer to determine who wins in a game of rock, paper, scissors. For a while I pondered how I could make the computer understand that 'rock' beats 'scissors' and so on and so forth. I figured the most straightforward way would be to use a switch and evaluate every scenario possible. My solution 'feels longwinded, but I guess it has to be that way since the computer can't interpret rules that aren't concrete.

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log('Hello world')
+// Create a function that will randomly return either 'rock', 'paper', or 'scissors' 
+function computerPlay () {
+    const options = ['rock', 'paper', 'scissors'];
+    const hand = options[Math.floor(Math.random() * options.length)];
+    return hand;
+}
+
+// Create a function that plays a single round of Rock Paper Scissors
+// It should take in playerSelection and computerSelection as parameters and return a string that declares the winner
+
+function playRound (playerSelection, computerSelection) {
+    // Creating a helper function to declare the winner
+    function declareWinner (playerSelection, computerSelection) {
+        return console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+    }
+
+    function declareLoser (playerSelection, computerSelection) {
+        return console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+    }
+
+    function declareDraw () {
+        return console.log(`It's a draw!`)
+    }
+
+    // Making the user's input case insensitive by turning their input lowercase regardless of how they typed it!
+    playerSelection = playerSelection.toLowerCase()
+    // Creating all the possible scenarios for if rock, paper, or scissors was chosen by the PLAYER
+    switch (playerSelection) {
+        // If the player chooses rock...
+        case 'rock':
+            if (computerSelection === playerSelection) {
+                declareDraw();
+            } else if (computerSelection === 'paper') {
+                declareLoser(playerSelection, computerSelection);
+            } else {
+                declareWinner(playerSelection, computerSelection);
+            }
+            break;
+        // If the player chooses paper...
+        case 'paper':
+            if (computerSelection === playerSelection) {
+                declareDraw();
+            } else if (computerSelection === 'scissors') {
+                declareLoser(playerSelection, computerSelection);
+            } else {
+                declareWinner(playerSelection, computerSelection);
+            }
+            break;
+        // If the player chooses scissors...
+        case 'scissors':
+            if (computerSelection === playerSelection) {
+                declareDraw();
+            } else if (computerSelection === 'rock') {
+                declareLoser(playerSelection, computerSelection)
+            } else {
+                declareWinner(playerSelection, computerSelection)
+            }
+            break;
+    }
+}
+
+const playerSelection = 'paper'
+playRound(playerSelection, computerPlay())
