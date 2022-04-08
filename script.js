cc101d484206478c135bf57f955a6e0bb90487dd
Essan'Utep Blue <eblue.codes@gmail.com>
2022-04-08 12:49:01 -0700

    Changed the application from being console based to having a UI that the user can interact with

--- script.js
+++ script.js
@@ -1,24 +1,85 @@
+// Create a counter to keep track of the player and computer's scores, and target the score text nodes so you can update the UI.
+let playerScore = 0;
+const playerScoreText = document.querySelector('#player-score');
+let computerScore = 0;
+const computerScoreText = document.querySelector('#computer-score');
+
+// Create a counter to keep track of the current round
+let roundCount = 1;
+const roundCountText = document.querySelector('#round')
+
+// Target the result-text div so we can display who won after a round is played.
+const resultText = document.querySelector('#result-text');
+
+// Target the button nodes using selectors so you can run functions after they are clicked.
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorBtn = document.querySelector('#scissors');
+
+// Now that the nodes have been targeted, I can run the game's functions if they are clicked.
+rockBtn.addEventListener("click", playerChoseRock);
+paperBtn.addEventListener("click", playerChosePaper);
+scissorBtn.addEventListener("click", playerChoseScissors);
+
 // Create a function that will randomly return either 'rock', 'paper', or 'scissors' 
 function computerPlay () {
     const options = ['rock', 'paper', 'scissors'];
-    const hand = options[Math.floor(Math.random() * options.length)];
-    return hand;
+    // Pick a random element from the options array using randomly generated index 
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
+    return computerChoice;
 }
 
 // Create a function that plays a single round of Rock Paper Scissors
 // It should take in playerSelection and computerSelection as parameters and return a string that declares the winner
 function playRound (playerSelection, computerSelection) {
-    // Creating a helper function to declare the winner
-    function declareWinner (playerSelection, computerSelection) {
-        return `You win! ${playerSelection} beats ${computerSelection}`
+
+
+    // Creating a helper function to declare the WINNER
+    function playerWin (playerSelection, computerSelection) {
+        // Increase the player's score by one if they win
+        playerScore+=1;
+        // Update the UI with the new player score
+        playerScoreText.textContent = playerScore;
+        // Update the UI with the new round number
+        nextRound()
+        // Update the UI with the results of the round
+        resultText.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        return;
     }
-    // Creating a helper function to declare the loser
-    function declareLoser (playerSelection, computerSelection) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+
+    // Creating a helper function to declare the LOSER
+    function playerLose (playerSelection, computerSelection) {
+        // Increase the computer's score by one if the player loses
+        computerScore+=1;
+        // Update the UI with the new computer's score
+        computerScoreText.textContent = computerScore;
+        // Update the UI with the new round number
+        nextRound();
+        // Update the UI with the results of the round
+        resultText.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
+        return;
     }
+
     // Creating a helper function to declare a draw!
     function declareDraw () {
-        return `It's a draw!`
+        resultText.textContent = "It's a draw!"
+        // Update the UI with the new round number
+        nextRound();
+        return;
+    }
+
+    // Creating a function to determine the winner based upon the player and computer's selection
+    function determineWinner(selection) {
+        // If the computer randomly chose the same option as the player did, it's a draw.
+        if (computerSelection === playerSelection) {
+            return declareDraw();
+            // If the computer randomly chose the option that beats the player's choice, the player loses.
+        } else if (computerSelection === selection) {
+            return playerLose(playerSelection, computerSelection);
+            // If the randomly chose the losing option, the player wins.
+        } else {
+            return playerWin(playerSelection, computerSelection);
+        }
     }
 
     // Making the user's input case insensitive by turning their input lowercase regardless of how they typed it!
@@ -27,43 +88,42 @@ function playRound (playerSelection, computerSelection) {
     switch (playerSelection) {
         // If the player chooses rock...
         case 'rock':
-            if (computerSelection === playerSelection) {
-                return declareDraw();
-            } else if (computerSelection === 'paper') {
-                return declareLoser(playerSelection, computerSelection);
-            } else {
-                return declareWinner(playerSelection, computerSelection);
-            }
-            break;
+            return determineWinner('paper');
         // If the player chooses paper...
         case 'paper':
-            if (computerSelection === playerSelection) {
-                return declareDraw();
-            } else if (computerSelection === 'scissors') {
-                return declareLoser(playerSelection, computerSelection);
-            } else {
-                return declareWinner(playerSelection, computerSelection);
-            }
-            break;
+            return determineWinner('scissors');
         // If the player chooses scissors...
         case 'scissors':
-            if (computerSelection === playerSelection) {
-                return declareDraw();
-            } else if (computerSelection === 'rock') {
-                return declareLoser(playerSelection, computerSelection)
-            } else {
-                return declareWinner(playerSelection, computerSelection)
-            }
+            return determineWinner('rock');
     }
 }
 
-
-
-function game() {
-    for (let i = 0; i<5; i++) {
-        let playerSelection = prompt('Please type one of the following options: rock, paper, or scissors', 'rock');
-        console.log(playRound(playerSelection, computerPlay()))
+function nextRound() {
+    if(roundCount < 5) {
+        roundCount+=1
+        roundCountText.textContent = roundCount;
+    } else {
+        prompt('GAME OVER')
+        roundCount = 0;
+        playerScore = 0;
+        computerScore = 0;
+        roundCountText.textContent = roundCount;
+        playerScoreText.textContent = playerScore;
+        computerScoreText.textContent = computerScore;
+        return;
     }
+    
 }
 
-game();
+function playerChoseRock() {
+    playRound('rock', computerPlay());
+}
+
+function playerChosePaper() {
+    playRound('paper', computerPlay());
+}
+
+function playerChoseScissors() {
+    playRound('scissors', computerPlay());
+}
+
